01c9d40801000f6551a0935bc160e44fc063771a
Fanny Rottee <frottee@tea-ebook.com>
2017-06-28 21:04:43 +0200

    Clean the code and add comments

--- library.js
+++ library.js
@@ -9,13 +9,11 @@ app.set('view engine', 'html');
 app.set('views',  __dirname +  '/views')
 app.use(bodyParser())
 
-// Récupération du client mongodb
 var mongoClient = require('mongodb').MongoClient;
 
-// Paramètres de connexion
 var url = 'mongodb://localhost:27017/library';
 
-//Affiche les livres
+//Return all the books
 app.get('/books', function(req, res) {
 	app.db.collection('book').find({}).toArray(function(err, book) {
 		res.render("library", {
@@ -24,26 +22,15 @@ app.get('/books', function(req, res) {
 	});
 });
 
-app.post('/books/new', function(req, res, next) {
+//Create a new book
+app.post('/books/new', function(req, res) {
 	var categories = req.body.categories.split(",");
 	var newBook = {ISBN: req.body.isbn, title: req.body.title, author: req.body.author, purchase_date: new Date(), condition: req.body.condition, categories: categories}
   	app.db.collection('book').save(newBook)
 	res.redirect('/books')
 });
 
-app.post('/books/:id/borrow', function(req, res) {
-  	app.db.collection('book').update({_id: new mongodb.ObjectId(req.params.id)}, { $set: {borrow: {borrow_date: new Date(), borrower: req.body.borrower}}})
-	res.redirect('/books/'+req.params.id)
-});
-
-app.post('/books/:id/return', function(req, res) {
-	if (req.body.return){
-		app.db.collection('book').update({_id: new mongodb.ObjectId(req.params.id)}, { $unset: {borrow: ''}})
-	}
-	res.redirect('/books/'+req.params.id)
-});
-
-
+// Return the selected book
 app.get('/books/:id', function(req, res) {
 	app.db.collection('book').find({_id: new mongodb.ObjectId(req.params.id)}).toArray(function(err, book) {
 		res.render("book", {
@@ -52,12 +39,26 @@ app.get('/books/:id', function(req, res) {
 	});
 });
 
+// Allow to borrow a book
+app.post('/books/:id/borrow', function(req, res) {
+  	app.db.collection('book').update({_id: new mongodb.ObjectId(req.params.id)}, { $set: {borrow: {borrow_date: new Date(), borrower: req.body.borrower}}})
+	res.redirect('/books/'+req.params.id)
+});
+
+// Allow to return a book
+app.post('/books/:id/return', function(req, res) {
+	if (req.body.return){
+		app.db.collection('book').update({_id: new mongodb.ObjectId(req.params.id)}, { $unset: {borrow: ''}})
+	}
+	res.redirect('/books/'+req.params.id)
+});
+
+//Delete the selected book
 app.get('/books/delete/:id', function(req, res) {
 	app.db.collection('book').remove({_id: new mongodb.ObjectId(req.params.id)})
 	res.redirect('/books')
 });
 
-// Connexion au serveur avec la méthode connect
 mongoClient.connect(url, function (err, db) {
 	app.db = db;
 	app.listen(8000);
